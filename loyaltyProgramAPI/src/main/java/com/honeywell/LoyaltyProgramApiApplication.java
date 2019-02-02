00bd3404da89dea68ece189e8451429d8b3b90ad
Vinodh Kumar Thimmisetty <thimmv>
2019-02-02 13:00:33 +0530

    Angular and Spring boot code setup

---
+++ loyaltyProgramAPI/src/main/java/com/honeywell/LoyaltyProgramApiApplication.java
@@ -0,0 +1,14 @@
+package com.honeywell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyProgramApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyProgramApiApplication.class, args);
+	}
+
+}
+
